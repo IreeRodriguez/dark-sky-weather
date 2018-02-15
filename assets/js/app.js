911ee6d95dc2c5abfec176640ad68a5db9438620
IreeRodriguez <ire208@gmail.com>
2018-02-14 22:46:41 -0300

    background de imagen random funcionando

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+// function getData(request) {
+//     fetch(request)
+//     .then(function(response) {
+//         let data = JSON.parse(this.responseText);
+//         console.log(data);
+//     })
+//     .then(function(text) {
+//         console.log('Request successful', text);
+//     })
+//     .catch(function(error) {
+//         console.log('Request failed', error)
+//     })    
+// }
+
+// getData('https://pokeapi.co/api/v2/pokemon-species/10/');
+
+// $.getJSON('https://pokeapi.co/api/v2/pokemon-species/10/', function(data) {
+//     console.log(data);
+    
+
+// });
